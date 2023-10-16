d4e166e4c502addee770a47e8f68f7cc1efa0c7b
Yassine <yassinemoussafir31@gmail.com>
2023-10-16 09:19:54 -0700

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * @str: string to be processed.
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int start;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		start = len / 2;
+	}
+	else
+	{
+		start = (len - 1) / 2;
+	}
+
+	for (int i = start; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
