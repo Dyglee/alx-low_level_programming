894f564bd0923e2c8534c604484741feda90179f
Yassine <yassinemoussafir31@gmail.com>
2023-10-18 05:20:29 -0700

    look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+/**
+ * string_toupper - Converts all lowercase letters of a string to uppercase.
+ * @s: The input string.
+ *
+ * Return: The converted string.
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
+
