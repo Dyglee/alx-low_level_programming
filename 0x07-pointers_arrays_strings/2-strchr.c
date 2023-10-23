f192d2fb5320d05e2b004f2f93de762697f8c32b
Yassine <yassinemoussafir31@gmail.com>
2023-10-23 12:32:42 -0700

    plz

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,16 +11,15 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+        int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
-		{
-			return (&s[i]);
-		}
-	}
+        for (i = 0; s[i] != '\0'; i++)
+        {
+                if (s[i] == c)
+                {
+                        return (&s[i]);
+                }
+        }
 
-	return (NULL);
+        return (NULL);
 }
-
